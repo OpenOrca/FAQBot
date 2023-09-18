156f0694a3782398bf1a66f70d96e7e6bcd32a6f
Eugene Pentland <eugenepentland@gmail.com>
2023-09-18 18:02:33 +0000

    passed all unit tests

--- src/database.py
+++ src/database.py
@@ -4,20 +4,44 @@ from src.config import POCKETBASE_URL
 
 pb = PocketBase(POCKETBASE_URL)
 
-def create_question(question, id):
-    cleaned_text = cleaned_message(question)
+def create_related_question(text, id, guild_id, channel_id, message_id, author_id, parent_question_id):
+    cleaned_text = cleaned_message(text)
     pb.collection('questions').create({
         'text': cleaned_text,
-        'id': id
+        'id': id,
+        'guild_id': guild_id,
+        'channel_id': channel_id,
+        'message_id': message_id,
+        'author_id': author_id,
+        "parent_question_id": parent_question_id
     })
 
-def create_answer(answer, id, question_id, source):
-    cleaned_text = cleaned_message(answer)
+def create_question(message, id, parent_question_id=""):
+    cleaned_text = cleaned_message(message.content)
+    pb.collection('questions').create({
+        'text': cleaned_text,
+        'id': id,
+        'guild_id': message.guild.id,
+        'channel_id': message.channel.id,
+        'message_id': message.id,
+        'author_id': message.author.id,
+        "parent_question_id": parent_question_id
+    })
+
+def create_answer(message, id, question_id, source, parent_answer_id = ""):
+    cleaned_text = cleaned_message(message.content)
+    if parent_answer_id:
+        parent_answer_id = parent_answer_id[:15]
     pb.collection('answers').create({
         'text': cleaned_text,
+        'id': id,
+        "parent_answer_id": parent_answer_id,
         'source': source,
         'question': question_id,
-        'id': id
+        'guild_id': message.guild.id,
+        'channel_id': message.channel.id,
+        'message_id': message.id,
+        'author_id': message.author.id
     })
 
 def update_answer_rating(id, rating):
@@ -29,14 +53,18 @@ def vector_search(question):
     cleaned_question = cleaned_message(question)
     results = pb.collection("questions").get_list(per_page=10000000, query_params={
         "search": cleaned_question,
+        "filter": 'parent_question_id = ""',
         "expand": "answers(question)"
     })
     if not results.items:
-        return
+        return None, None
     first_question = results.items[0]
 
     del first_question.embedding
-    first_answer = pb.collection('answers').get_first_list_item(f"question = '{first_question.id}'", query_params= {
-       "sort":"-rating"
+    first_answer = pb.collection('answers').get_first_list_item(f"question = '{first_question.id}' && parent_answer_id = ''", query_params= {
+       "sort":"-rating",
     })
-    return first_question, first_answer
+    if first_answer:
+        return first_question, first_answer
+    else:
+        return first_answer, None
--- src/event_handlers.py
+++ src/event_handlers.py
@@ -1,19 +1,19 @@
 from src.utils import get_message_id, cleaned_message, get_answer_rating
-from src.database import (create_question, create_answer, vector_search,
+from src.database import (create_question, create_answer, vector_search, create_related_question,
                       update_answer_rating, pb)
 from src.llm import get_llm_message
 
 async def create_reply(message):
     clean_content = cleaned_message(message.content)
-    message_id = get_message_id(message)
+    question_id = get_message_id(message)
 
     if len(clean_content) < 1:
         return
 
-    create_question(clean_content, message_id)
+    create_question(message, question_id)
     response_message = await get_llm_message(message)
     response_message_id = get_message_id(response_message)
-    create_answer(response_message.content, response_message_id, message_id, 'llm')
+    create_answer(response_message, response_message_id, question_id, 'llm')
 
 
 async def get_parent_message(message):
@@ -26,14 +26,14 @@ async def handle_reaction(type, reaction, user, client):
     if user == client.user:
         return
 
-    cleaned_text = cleaned_message(reaction.message.content)
     if str(reaction.emoji) == "👍" or str(reaction.emoji) == "👎":
-        try:
-            answer_row = pb.collection('answers').get_first_list_item(f"text = '{cleaned_text}'")
-            rating = get_answer_rating(reaction, type)
-            update_answer_rating(answer_row.id, rating)
-        except:
-            print('Row not found')
+        message_id = get_message_id(reaction.message)
+        answer_row = pb.collection('answers').get_one(message_id)
+        rating = get_answer_rating(reaction, type)
+        answer_id = answer_row.id
+        if answer_row.parent_answer_id:
+            answer_id = answer_row.parent_answer_id
+        update_answer_rating(answer_id, rating)
 
     if str(reaction.emoji) == "❓":
         await create_reply(reaction.message)
@@ -43,34 +43,52 @@ async def handle_on_message(message, client):
     parent_message = await get_parent_message(message)
     message_id = get_message_id(message)
 
-    if client.user in message.mentions:
-        if not parent_message:
-            await handle_bot_mention_in_new_message(message, client)
-        else:
-            await handle_bot_mention_in_reply(message, client, parent_message, message_id)
+    if not parent_message:
+    # Generates a reply from the model or pulls from vector database
+        if client.user in message.mentions:
+                await handle_bot_mention_in_new_message(message)
+    else:
+        # Adds reaction to users answers for voting
+        await handle_bot_mention_in_reply(message, client, parent_message, message_id)
 
 
-async def handle_bot_mention_in_new_message(message, client):
+async def handle_bot_mention_in_new_message(message):
     vector_db_question, vector_db_answer = vector_search(message.content)
+    # When the question is similar to something its seen before
     if vector_db_question and vector_db_question.similarity > 0.8:
         response = vector_db_answer.text
         pb.collection('questions').update(vector_db_question.id, {"times_asked+": 1})
         bot_message = await message.reply(content=response)
+        message_id = get_message_id(bot_message)
+        # Checks if there is a parent
+        base_message_id = get_message_id(message)
+        create_related_question(vector_db_question.text, base_message_id, vector_db_question.guild_id, vector_db_question.channel_id, vector_db_question.message_id, vector_db_question.author_id, vector_db_question.id)
+        create_answer(bot_message, message_id, vector_db_answer.question, vector_db_answer.source, vector_db_answer.id)
         await bot_message.add_reaction("👍")
         await bot_message.add_reaction("👎")
+    # When the model is being used to generate the output
     else:
         await create_reply(message)
 
 
 async def handle_bot_mention_in_reply(message, client, parent_message, message_id):
-    parent_id = get_message_id(parent_message)
+    #Checks to see if there is a parent question
+    question_id = get_message_id(parent_message)
+    id = message_id
+
+    # When the user replies to a question with @bot
     if client.user in parent_message.mentions:
-        create_answer(message.content, message_id, parent_id, 'user')
+        db_question = pb.collection('questions').get_one(question_id)
+        if db_question.parent_question_id:
+            question_id = db_question.parent_question_id
+        # Check to see if the parent message has parent
+        create_answer(message, id, question_id, 'user')
         await message.add_reaction("👍")
         await message.add_reaction("👎")
 
+    # When the user provides an answer and adds the bot to the message
     if client.user in message.mentions:
-        create_question(parent_message.content, parent_id)
-        create_answer(message.content, message_id, parent_id, 'user')
+        create_question(parent_message, question_id)
+        create_answer(message, id, question_id, 'user')
         await message.add_reaction("👍")
         await message.add_reaction("👎")
